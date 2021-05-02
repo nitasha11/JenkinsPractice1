e2194dcca6ba0bf8294888e7de5c1f428f5df7c9
nitasha11 <51523275+nitasha11@users.noreply.github.com>
2021-05-02 23:55:38 +0530

    Update multiWindow.java

--- multiWindow.java
+++ multiWindow.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class multiWindow {
-
+//updating version
 	public static void main(String[] args)  {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
 		//System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
